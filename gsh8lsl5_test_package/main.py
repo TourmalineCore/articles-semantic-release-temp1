442ce3570a77070e7b29a58a9198d9f13be153a2
Artem Sheptunov <106321977+aasheptunov@users.noreply.github.com>
2026-07-17 14:03:34 +0500

    feat: add sum function (#29)
    Closes #1
    
    ---
    
    Co-authored-by: Artem Sheptunov <106321977+Infindery@users.noreply.github.com>

--- gsh8lsl5_test_package/main.py
+++ gsh8lsl5_test_package/main.py
@@ -1,12 +1,11 @@
-def sub(a: int, b: int) -> int:
-    return a - b
+# def sub(a: int, b: int) -> int:
+#     return a - b
 
 def sum(a: int, b: int) -> int:
     return a + b
 
 def main() -> None:
     return 0
-
 
 if __name__ == "__main__":
     main()
