a04b9ab340fc5b38c3d10bbcdf972ec9a583a49a
Artem Sheptunov <sheptunov-aa@mail.ru>
2026-07-29 12:57:30 +0500

    ci: update release branches config

--- release.config.cjs
+++ release.config.cjs
@@ -3,8 +3,8 @@ const { RELEASE_RULES, CHANGELOG_TYPES } = require('./release.rules.cjs');
 module.exports = {
     // You can find out more about the configuration of this file here https://semantic-release.gitbook.io/semantic-release/usage/configuration
     "branches": [
-        { "name": 'master' },
-        { "name": '*/master', "range": '${name.match(/^(\\d+)\\.x/)[1]}.x' },
+        { "name": '1.x/master', "range": '1.x' }, 
+        { "name": 'master' }, 
         { "name": '{release/*,*/release/*}', "prerelease": 'rc' },
         { "name": '{develop,*/develop}', "prerelease": 'alpha' },
     ],
